c460254e9f83c24f9984fefd11cb642618f9e175
tauraloke <tauraloke@gmail.com>
2022-11-04 20:44:06 +0300

    add hash index to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_141402) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_174107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_141402) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", using: :hash
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
